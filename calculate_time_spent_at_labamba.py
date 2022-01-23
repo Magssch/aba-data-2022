c362aa20fbc6ebf14971fef8062fedb4e91ee2dc
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2022-01-23 19:10:13 +0100

    Add total labamba time script

---
+++ calculate_time_spent_at_labamba.py
@@ -0,0 +1,23 @@
+import json
+
+labamba_event_list = None
+total_labamba_time = 0
+
+with open("results/labamba_events_list.json", "r") as fp:
+    labamba_event_list = json.load(fp)["events"]
+
+for event in labamba_event_list:
+    start_time = event["startTime"].split("T")[1].split(":")[:2]
+    end_time = event["endTime"].split("T")[1].split(":")[:2]
+    for i in range(2):
+        start_time[i] = int(start_time[i])
+        end_time[i] = int(end_time[i])
+    if end_time[0] < 5:
+        end_time[0] += 24
+    print(f"{start_time} {end_time}")
+    duration = ((end_time[0] - start_time[0])*60) - start_time[1] + end_time[1]
+    print(duration)
+    total_labamba_time += duration
+
+print(f"Total LaBamba time in minutes: {total_labamba_time}")
+
